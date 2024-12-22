0612b0c51da72542afb3fc047f873841e2b3c6c2
MinDeaDBlood <151534222+MinDeaDBlood@users.noreply.github.com>
2024-12-22 18:18:58 +0300

    Update run_russian.py

--- run_russian.py
+++ run_russian.py
@@ -16,25 +16,49 @@ import banner
 import ext4
 from Magisk import Magisk_patch
 import os
-import logging  # Импортируем модуль для логирования
 
 from dumper import Dumper
 
-# Настройка логирования
-logging.basicConfig(
-    filename='error_log.txt',  # Имя файла для логов
-    level=logging.INFO,  # Уровень логирования
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-
 if os.name == 'nt':
     import ctypes
+
     ctypes.windll.kernel32.SetConsoleTitleW("TIK5_Alpha")
 else:
     sys.stdout.write("\x1b]2;TIK5_Alpha\x07")
     sys.stdout.flush()
+import extract_dtb
+import requests
+from rich.progress import track
+import contextpatch
+import downloader
+import fspatch
+import imgextractor
+import lpunpack
+import mkdtboimg
+import ofp_mtk_decrypt
+import ofp_qc_decrypt
+import ozipdecrypt
+import utils
+from api import cls, dir_has, cat, dirsize, re_folder, f_remove
+from log import LOGS, LOGE, ysuc, yecho, ywarn
+from utils import gettype, simg2img, call
+import opscrypto
+import zip2mpk
+from rich.table import Table
+from rich.console import Console
 
-# Класс для редактирования JSON-файлов
+LOCALDIR = os.getcwd()
+binner = o_path.join(LOCALDIR, "bin")
+setfile = o_path.join(LOCALDIR, "bin", "settings.json")
+platform = plat.machine()
+ostype = plat.system()
+if os.getenv('PREFIX'):
+    if os.getenv('PREFIX') == "/data/data/com.termux/files/usr":
+        ostype = 'Android'
+ebinner = o_path.join(binner, ostype, platform) + os.sep
+temp = o_path.join(binner, 'temp')
+
+
 class json_edit:
     def __init__(self, j_f):
         self.file = j_f
@@ -57,42 +81,50 @@ class json_edit:
         data[name] = value
         self.write(data)
 
-# Функция для удаления директорий
+
 def rmdire(path):
-    if os.path.exists(path):
+    if o_path.exists(path):
+        if os.name == 'nt':
+            for r, d, f in os.walk(path):
+                for i in d:
+                    if i.endswith('.'):
+                        call('mv {} {}'.format(os.path.join(r, i), os.path.join(r, i[:1])))
+                for i in f:
+                    if i.endswith('.'):
+                        call('mv {} {}'.format(os.path.join(r, i), os.path.join(r, i[:1])))
+
         try:
             shutil.rmtree(path)
         except PermissionError:
-            logging.warning("Не удается удалить папку, недостаточно разрешений")
+            ywarn("Не удается удалить папку, недостаточно разрешений")
         else:
-            logging.info("Удалено успешно!")
+            ysuc("Удалено успешно!")
 
-# Обработчик ошибок
+
 def error(exception_type, exception, traceback):
     cls()
     table = Table()
     try:
         version = settings.version
-    except AttributeError:
+    except:
         version = 'Неизвестна'
     table.add_column(f'[red]Ошибка:{exception_type.__name__}[/]', justify="center")
     table.add_row(f'[yellow]Описание:{exception}')
-    table.add_row(f'[yellow]Строки:{exception.__traceback__.tb_lineno}\tМодуль:{exception.__traceback__.tb_frame.f_globals["__name__"]}')
+    table.add_row(
+        f'[yellow]Строки:{exception.__traceback__.tb_lineno}\tМодуль:{exception.__traceback__.tb_frame.f_globals["__name__"]}')
     table.add_section()
-    table.add_row(f'[blue]Платформа:[purple]{plat.machine()}\t[blue]Система:[purple]{plat.uname().system} {plat.uname().release}')
+    table.add_row(
+        f'[blue]Платформа:[purple]{plat.machine()}\t[blue]Система:[purple]{plat.uname().system} {plat.uname().release}')
     table.add_row(f'[blue]Питон:[purple]{sys.version[:6]}\t[blue]Версия программы:[purple]{version}')
     table.add_section()
     table.add_row(f'[green]Отчет об ошибках:https://github.com/ColdWindScholar/TIK/issues')
     Console().print(table)
-    
-    # Логируем ошибку
-    logging.error(f'{exception_type.__name__}: {exception}, строка: {exception.__traceback__.tb_lineno}, модуль: {exception.__traceback__.tb_frame.f_globals["__name__"]}')
-    
     input()
     sys.exit(1)
 
-# Устанавливаем обработчик исключений на уровень всего приложения
-sys.excepthook = error
+
+# sys.excepthook = error
+
 
 def sha1(file_path):
     if os.path.exists(file_path):
@@ -1892,8 +1924,4 @@ def autounpack(project):
 
 
 if __name__ == '__main__':
-    try:
-        Tool().main()
-    except Exception as ex:
-        logging.error(f"Критическая ошибка в программе: {str(ex)}")
-        print("Произошла ошибка. Проверьте логи для подробной информации.")
+    Tool().main()
